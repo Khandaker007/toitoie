8813fa7ad3f17ba587d08ab13f6cb5e5b2b7443f
Dewan Wakil Ahmed <dewanwakilahmed@gmail.com>
2022-04-04 03:09:55 +0600

    Created Server Entry Point

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log("Hello world!");
+const express = require("express");
+const dotenv = require("dotenv").config();
+
+// Initialize App
+const app = express();
+
+// Serve on Home Root
+app.get("/", (req, res) => {
+  res.send("Toitoie Server is running and is ready to process requests...");
+});
+
+// PORT
+const PORT = process.env.PORT || 5000;
+
+// Listen for Requests
+app.listen(PORT, () => {
+  console.log(`Toitoi Server is running on PORT: ${PORT}...`);
+});
