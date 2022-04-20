565d489c07b6a82e43b985f97b38cb76a47ed48a
Dewan Wakil Ahmed <dewanwakilahmed@gmail.com>
2022-04-20 23:54:44 +0600

    Completed registerUser Controller

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -1,9 +1,57 @@
+const asyncHandler = require("express-async-handler");
+const bcrypt = require("bcryptjs");
+
+// Models
+const User = require("../models/user.model");
+
+// Utils
+const generateJWT = require("../utils/generateJWT.util");
+
 // @desc    Register User
 // @route   POST api/user/
 // @access  Public
-const registerUser = (req, res) => {
-  res.send("Register User Route Reached!");
-};
+const registerUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+
+  // Check Required Fields
+  if (!email || !password) {
+    res.status(400);
+    throw new Error("Please add all required fields!");
+  }
+
+  // Check if Email is already taken
+  const userWithEmail = await User.findOne({ email });
+  if (userWithEmail) {
+    res.status(400);
+    throw new Erorr("Email is already taken!");
+  }
+
+  // Hash Password
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+
+  // Create User
+  const user = await User.create({
+    email,
+    password: hashedPassword,
+  });
+
+  if (user) {
+    console.log(`User Registered. Email: ${user.email}`.brightGreen.underline);
+
+    res.status(201).json({
+      id: user.id,
+      email: user.email,
+      token: generateJWT(user.id),
+    });
+  } else {
+    console.log(
+      `User could NOT be Registered. Email: ${email}`.brightRed.underline
+    );
+    res.status(400);
+    throw new Error("SERVER ERROR: User Registration NOT Successful!");
+  }
+});
 
 // @desc    Login User
 // @route   POST api/user/login
